472292f522cc09e5d0a2f0e8355067165ec4e0e9
Faith Philemon <philemonfaith@gmail.com>
2019-10-10 18:27:13 +0100

    make header/footer urls dynamic

--- application/views/layouts/mainview.php
+++ application/views/layouts/mainview.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?=$title ?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url("css/bootstrap.min.css"); ?>">
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.css">
-    <link rel="stylesheet" href="css/style.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/fontawesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/dashboard.css">
 </head>
 <body> 
     <main>
@@ -19,16 +19,16 @@
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-<!--               <a class="navbar-brand" href="#"><img src="images/MAUTECH-Post-UTME-Form-o3schools.jpeg" width="50" height="50" alt=""></a>
+<!--               <a class="navbar-brand" href="<?php echo base_url() ?>#"><img src="<?php echo base_url() ?>images/MAUTECH-Post-UTME-Form-o3schools.jpeg" width="50" height="50" alt=""></a>
  -->              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="<?php echo base_url() ?>index.html">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
               </ul>
               <form class="form-inline my-2 my-lg-0 mr-5">
                 <div class="dropdown">
-                    <a href="" id="dropdownMenu2"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a href="<?php echo base_url() ?>" id="dropdownMenu2"   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     </a>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
                       <button class="dropdown-item" type="button">Log out</button>
@@ -43,7 +43,7 @@
               <div class="container">
                 <div class="MainHeading header">
                   <span>
-                    <img src="images/MAUTECH-Post-UTME-Form-o3schools.jpeg" width="110" height="100">
+                    <img src="<?php echo base_url() ?>images/MAUTECH-Post-UTME-Form-o3schools.jpeg" width="110" height="100">
                   </span>
                     <h4><b>welcome to the department of statistic & operations research <br> project Database management system </b></h4>
                 </div>
@@ -67,9 +67,9 @@
         </div>
     </footer>
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js"></script>
-<script src="js/fontawesome.min.js"></script>
+<script src="<?php echo base_url() ?>js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>https://unpkg.com/bootstrap-table@1.15.4/dist/bootstrap-table.min.js"></script>
+<script src="<?php echo base_url() ?>js/fontawesome.min.js"></script>
 </body>
 </html>

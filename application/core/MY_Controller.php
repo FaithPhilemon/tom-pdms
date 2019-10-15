c73e744f9b6c252a865f15c3e1e1c36e99cf95b0
Faith Philemon <philemonfaith@gmail.com>
2019-10-16 00:54:39 +0100

    set base class

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -24,4 +24,30 @@ class Admin_Controller extends MY_Controller {
 	// 	}
 	// }
 
+	public function upload_document($topic){
+    	// uploads/users
+        $config['upload_path'] = 'uploads/documents';
+        $config['file_name'] = $topic;
+        $config['allowed_types'] = 'docx|doc|pdf';
+        $config['max_size'] = '1024';
+
+        // $config['max_width']  = '1024';
+        // $config['max_height']  = '768';
+
+        $this->load->library('upload', $config);
+        if ( ! $this->upload->do_upload('document'))
+        {
+            $error = $this->upload->display_errors();
+			return $error;
+
+        }else{
+            $data = array('upload_data' => $this->upload->data());
+            $type = explode('.', $_FILES['document']['name']);
+            $type = $type[count($type) - 1];
+            
+			$path = $config['upload_path'].'/'.$config['file_name'].'.'.$type;
+            return ($data == true) ? $path : false;            
+        }
+	}
+
 }
